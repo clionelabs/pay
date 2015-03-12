09424020cb5ae02588c6d64a0b496fddc053994c
Wat Chun Pang, Gilbert <gilbert.wat@getthingsdone.hk>
2015-03-12 10:09:23 +0800

    Refactored a bit

--- server/router.es6.js
+++ server/router.es6.js
@@ -6,8 +6,8 @@ Router.route('template_previews/reviewing', {
   where : 'server'
 });
 
-Router.route('webhooks/init', {
-  name: 'webhooks_init',
+Router.route('webhooks/bill_received', {
+  name: 'webhooks_bill_received',
   action() {
     let formidable = Meteor.npmRequire('formidable');
     let util = Meteor.npmRequire('util');
@@ -15,32 +15,6 @@ Router.route('webhooks/init', {
     let postInfo = new formidable.IncomingForm();
     let request = this.request;
     let response = this.response;
-    Async.runSync(() => {
-      postInfo.parse(request, (err, fields, files) => {
-
-        let check = (api_key, token, timestamp, signature) => {
-          let crypto = Meteor.npmRequire('crypto');
-          let hmac = crypto.createHmac('SHA256', api_key);
-
-          return signature === hmac.update(timestamp + token).digest('hex');
-        };
-
-        if (!check("key-b4559e5d4df905e6711f1deb3cd4c0f2", fields.token, fields.timestamp, fields.signature)) {
-          response.writeHead(406, {});
-          response.end();
-          //TODO add log
-          return;
-        }
-
-        response.writeHead(200, {'content-type': 'text/plain'});
-        response.end('');
-      });
-    });
-
-    let generateEmailHeader = (request) => {
-
-    };
-
 
     if (this.request.method !== 'POST') {
       this.response.writeHead(403, {});
@@ -49,9 +23,28 @@ Router.route('webhooks/init', {
       return;
     }
 
-    //Email.send();
-    this.response.writeHead(200, {'Content-Type' : 'text/html'})
-    this.response.end();
+    Async.runSync(() => {
+      postInfo.parse(request, (err, fields, files) => {
+        let apiKey = Meteor.settings.email.mailgun.apiKey;
+        let token = fields.token;
+        let ts = fields.timestamp;
+        let signature = fields.signature;
+
+        if (!Email.validateMailgun(apiKey, token, ts, signature)) {
+          response.writeHead(406, {});
+          response.end();
+          //TODO add log
+          return;
+        }
+
+        //TODO Email.send();
+
+        //console.log(util.inspect({ "fields" : fields , "files" : files}));
+        response.writeHead(200, {'content-type': 'text/plain'});
+        response.end('');
+      });
+    });
+
   },
   where : 'server'
 });
